42af4238d44ee12633c41648833e9f002728e6e9
bhim762 <deepakkumar639455@gmail.com>
2026-09-15 14:09:05 +0530

    Time: 0 ms (100.00%) | Memory: 43.2 MB (63.58%) - LeetSync

---
+++ 68-text-justification/text-justification.java
@@ -0,0 +1,60 @@
+import java.util.*;
+
+class Solution {
+    public List<String> fullJustify(String[] words, int maxWidth) {
+        List<String> result = new ArrayList<>();
+        int i = 0;
+        int n = words.length;
+
+        while (i < n) {
+            // Step 1: determine how many words fit on this line
+            int lineLen = words[i].length();
+            int j = i + 1;
+
+            while (j < n && lineLen + 1 + words[j].length() <= maxWidth) {
+                lineLen += 1 + words[j].length();
+                j++;
+            }
+
+            int numWords = j - i;
+            StringBuilder line = new StringBuilder();
+
+            if (j == n || numWords == 1) {
+                // Last line OR single word on the line -> left justify
+                for (int k = i; k < j; k++) {
+                    line.append(words[k]);
+                    if (k < j - 1) line.append(' ');
+                }
+                // pad with trailing spaces
+                while (line.length() < maxWidth) {
+                    line.append(' ');
+                }
+            } else {
+                // Fully justify: distribute spaces evenly
+                int totalChars = 0;
+                for (int k = i; k < j; k++) {
+                    totalChars += words[k].length();
+                }
+                int totalSpaces = maxWidth - totalChars;
+                int gaps = numWords - 1;
+                int spacesEach = totalSpaces / gaps;
+                int extraSpaces = totalSpaces % gaps;
+
+                for (int k = i; k < j; k++) {
+                    line.append(words[k]);
+                    if (k < j - 1) {
+                        int spacesToAdd = spacesEach + (k - i < extraSpaces ? 1 : 0);
+                        for (int s = 0; s < spacesToAdd; s++) {
+                            line.append(' ');
+                        }
+                    }
+                }
+            }
+
+            result.add(line.toString());
+            i = j;
+        }
+
+        return result;
+    }
+}
